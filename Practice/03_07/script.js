8569d3d94193eb4085d2648b9e2f336855426063
jlbrod <109424471+jlbrod@users.noreply.github.com>
2022-07-31 14:18:08 +1000

    Practice 1

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,30 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+const bottle = {
+  name: "water bottle",
+  volume: "500mls",
+  colour: "silver",
+  lidOpen: false,
+  toggleLid: function (LidStatus) {
+    this.lidOpen = LidStatus;
+  },
+  insulated: true,
+};
+
+const refrigerator = {
+  name: "car fridge",
+  volume: "40 litres",
+  colour: "white",
+  can: {
+    volume: "500mls",
+    material: "aluminum",
+    height: "200mm",
+    empty: false,
+    drinkContents: function (CanContents) {
+      this.empty = CanContents;
+    },
+
+    sugarfree: false,
+  },
+};
